46593e5676ce3f446cbeca09b403fe996e41475a
kim <90502548+kimmee01@users.noreply.github.com>
2022-10-28 13:42:43 +0700

    add gitignore

---
+++ my-vite-template/src/plugins/axios.js
@@ -0,0 +1,19 @@
+import defaultAxios from '@/lib/axios.js'
+import { useAppStore } from '@/store/app-store.js'
+import { useErrorMsg } from '@/store/error-msg-store'
+export const install = ({ app, router }) => {
+  defaultAxios.interceptors.response.use(res => {
+    // // Do something before request is sent
+    // if (res.data?.token === '') {
+    //   useAppStore().setToken('')
+    //   router.replace('/signin')
+    // }
+    // useErrorMsg().close()
+    // if (!res.data.ok) {
+    //   useErrorMsg().updateMsgModal(res.data.error)
+    // }
+    return res
+  })
+  app.config.globalProperties.$axios = defaultAxios
+  app.config.globalProperties.$http = defaultAxios
+}
